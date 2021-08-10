88cc3e276427215f15b84a510168b0cd9aed0684
lingi524 <lingi524@student.liu.se>
2021-08-10 11:58:01 +0200

    Fixed some typography. Time for lunch!

--- src/components/global-component/bookBox.js
+++ src/components/global-component/bookBox.js
@@ -8,6 +8,11 @@ function BookBox() {
         <div className="BookBox">
             <div className="BookBoxRectangle">
                 <img className="BookBoxBookCover" src={BookTemplate} alt="The book we're reading this month" />
+                <div className="BookBoxRectangleContainer">
+
+                <p className="BookTitle">The Haunting of Hill House</p>
+                <p className="BookAuthor">Shirley Jackson</p>
+                </div>
             </div>
         </div>
     )
