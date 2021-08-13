dde3c308a8fe294449052bcf8665a0094700e792
lingi524 <lingi524@student.liu.se>
2021-08-13 11:18:04 +0200

    Removed the wonky negative margin and made it position aboslut instead. Also added some text in footer

--- src/components/global-component/footer.js
+++ src/components/global-component/footer.js
@@ -6,8 +6,8 @@ function Footer() {
         <div className="Footer">
             <p><span id="boldFooter">Made by <a href="https://github.com/lingi524">Linnéa</a> with love</span><br />
                 Coded with React<br />
-                Powered on Contentful<br />
-                Deployed on Netlify
+                (Soon to be) Powered on Contentful<br />
+                (Soon to be) Deployed on Netlify
             </p>
         </div>
     )
