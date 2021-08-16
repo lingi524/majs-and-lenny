7f7c80143450853a40776850b5e31d23b0d0c34c
lingi524 <lingi524@student.liu.se>
2021-08-16 11:27:23 +0200

    All mobile layout is done

--- src/components/read-book-page/reviewPage.js
+++ src/components/read-book-page/reviewPage.js
@@ -5,6 +5,8 @@ import Menu from "../global-component/menu";
 import BookTemplate from "../images/bookTemplateHolder.jpeg"
 import MightAlsoLike from "./mightAlsoLike";
 
+var reviewText = "Dr. Montague, an occult scholar, and his team--Theodora, his assistant; Eleanor, a fragile young woman with a close acquaintance with the paranormal; and Luke, the future heir of Hill House--conduct an unusual research project in a reputedly haunted house. ";
+
 
 
 function ReviewPage() {
@@ -17,23 +19,23 @@ function ReviewPage() {
             <h1>The Haunting of Hill House</h1>
             <h2>Shirely Jackson</h2>
             <img className="BookPageBook" src={BookTemplate} alt="The book we're reading this month" />
+            <p className="ReviewText">{reviewText}</p>
             <div className="ReviewPageRectangle">
-                <p>
-                   <span>
-                    Author origin
-                    Year of publication
-                    Number of pages
-                    What genre is it?
-                    Who decided? 
-                    </span>
-                    <span className="BodyText">
-                    America
-                    1959
-                    246
-                    Gothic Horror
-                    Maja
-                    </span>
-                </p>
+                
+                   <p className="BookInfo">
+                    <span>Authors origin?</span>
+                    <span>Year of publication?</span>
+                    <span>Number of pages?</span>
+                   <span>What genre is it?</span>
+                    <span>Who decided?</span>
+                    </p>
+                    <p className="BookInfoAnswers">
+                    <span>America</span>
+                    <span>1959</span>
+                    <span>246</span>
+                    <span>Gothic Horror</span>
+                    <span>Maja</span>
+                    </p>
             </div>
             </div>
             <Button />
