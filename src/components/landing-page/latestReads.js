e1a24818e1def151bb6f797bad98c0ca49521b6e
lingi524 <lingi524@student.liu.se>
2021-08-25 13:20:26 +0200

    Fixed the filterting for read and tbr on landing page. thnx Neha

--- src/components/landing-page/latestReads.js
+++ src/components/landing-page/latestReads.js
@@ -4,25 +4,41 @@ import Button from "../global-component/button";
 
 function LatestReads({bookBoxData}) {
 
-    const items = [];
-    {
-        for (var i = 0; i < 4; i++) {
-        items.push(
-        <BookBox 
-        key={i}
-        title={bookBoxData[i].title} 
-        author={bookBoxData[i].auhtor}
-        image = {bookBoxData[i].bookCover.url}
-        grade={"1"}
-        />
-      )}
-    }
+    const readBooks = bookBoxData.filter(b => b.read)
+
+    // const items = [];
+    // {
+    //     for (var i = 0; i < 4; i++) {
+    //         if (bookBoxData[i].read === true) {
+    //     items.push(
+    //     <BookBox 
+    //     key={i}
+    //     title={bookBoxData[i].title} 
+    //     author={bookBoxData[i].auhtor}
+    //     image = {bookBoxData[i].bookCover.url}
+    //     grade={"1"}
+    //     />
+    //   )} }
+    // }
 
     return (
         <div className="LatestReads">
             <h2>Our latest reads</h2>
             <div className="BookBoxContainer">
-                {items}
+            {readBooks.map((b, index) => {
+                   if (index < 4) {
+                       return (
+                        <BookBox 
+                        key={index}
+                        title={b.title} 
+                        author={b.author}
+                        image = {b.bookCover.url}
+                        grade={"1"}
+                        />
+                        )
+                   }
+               }
+             )}
             </div>
             <Button />
         </div>
--- src/components/landing-page/onTheTBR.js
+++ src/components/landing-page/onTheTBR.js
@@ -5,28 +5,27 @@ import Button from "../global-component/button";
 function OnTheTBR({bookBoxData}) {
     
 
-    const items = [];
-    {
-        for (var i = 0; i < 4; i++) {
-         if (bookBoxData[i].read === false) {
-            items.push(
-            <BookBox 
-            key={i}
-            title={bookBoxData[i].title} 
-            author={bookBoxData[i].auhtor}
-            image = {bookBoxData[i].bookCover.url}
-            grade={"1"}
-            />
-            )  
-             }
-        }
-    }
+    const undreadBooks = bookBoxData.filter(b => !b.read)
+    console.log(undreadBooks)
 
     return (
         <div className="OnTheTBR">
             <h2>What’s on our TBR?</h2>
             <div className="BookBoxContainer">
-               {items}
+               {undreadBooks.map((b, index) => {
+                   if (index < 4) {
+                       return (
+                        <BookBox 
+                        key={index}
+                        title={b.title} 
+                        author={b.author}
+                        image = {b.bookCover.url}
+                        grade={"1"}
+                        />
+                        )
+                   }
+               }
+             )}
             </div>
             <Button />
         </div>
