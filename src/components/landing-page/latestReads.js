ab96414d6d4e75da3c0f47c2b2c9fa2bd26cbb22
lingi524 <lingi524@student.liu.se>
2021-09-01 18:00:49 +0200

    Tried to add a window resize event to determine number of bookboxes. Does not work

--- src/components/landing-page/latestReads.js
+++ src/components/landing-page/latestReads.js
@@ -5,43 +5,32 @@ import Button from "../global-component/button";
 function LatestReads({bookBoxData}) {
 
     const readBooks = bookBoxData.filter(b => b.read);
+    var bookAmount;
 
-    // (window).resize(function() {
-    //     var width = (window).width();
-    //     var bookAmount;
+    function windowSize() {
+        var width = window.innerWidth;
     
-    //     if (width <600 && width>768) {
-    //         bookAmount = 3;
-    //     } else if (width <= 599) {
-    //         bookAmount = 4;
-    //     } else {
-    //         bookAmount = 4;
-    //     }
-    
-    // });
+        if (width >600) {
+            bookAmount = 3;
+        } else if (width <599) {
+            bookAmount = 4;
+        } else {
+            bookAmount = 4;
+        }
+            console.log(width)
+            console.log(bookAmount)
+        };
+            
+        window.addEventListener('resize', windowSize);
   
-
-    // const items = [];
-    // {
-    //     for (var i = 0; i < 4; i++) {
-    //         if (bookBoxData[i].read === true) {
-    //     items.push(
-    //     <BookBox 
-    //     key={i}
-    //     title={bookBoxData[i].title} 
-    //     author={bookBoxData[i].auhtor}
-    //     image = {bookBoxData[i].bookCover.url}
-    //     grade={"1"}
-    //     />
-    //   )} }
-    // }
 
     return (
         <div className="LatestReads">
             <h2>Our latest reads</h2>
             <div className="BookBoxContainer">
+                {}
             {readBooks.map((b, index) => {
-                   if (index < 4) {
+                   if (index < {bookAmount}) {
                        return (
                         <BookBox 
                         key={index}
